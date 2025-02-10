49e28ad7957b28e712d8c6940ba0fbed9bca0168
Sanjan Athyady <sanjanathyady8@gmail.com>
2025-02-10 11:41:58 +0530

    feat: sidebar completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/sidebar"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Layout({ children }) {
   return (
-    <></>
+    <>
+   <div className="container flex flex-col items-center justify-center  ">
+    <Button>
+    <Link href="/dashboard">Home</Link>
+    </Button>
+   </div>
+    </>
   )
 }
